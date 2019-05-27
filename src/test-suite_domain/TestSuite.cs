3fb3bea1385e29a0ad1ecc899c1823750a8996d8
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-27 21:26:20 +0100

    Test that one book is exempt (not building)

---
+++ src/test-suite_domain/TestSuite.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace SalesTaxes.TestSuite.Domain
+{
+    public class TestSuite
+    {
+        [Fact]
+        public void OneBookIsExempt()
+        {
+            var counter = new Counter();
+            var article = new Article { Id = 1, Name = "Gone with the wind", Price = 25.0M };
+            counter.Scan(article);
+            var receipt = counter.EmitReceipt();
+            Assert.Equal(article.Price, receipt.Entries.SingleOrDefault()?.Price);
+        }
+    }
+}
