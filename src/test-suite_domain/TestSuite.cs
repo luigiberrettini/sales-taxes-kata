39ed9f620e179f5c7ff6d19b5d3162c809db51fc
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-27 21:32:02 +0100

    Add empty article, counter, receipt, entry (test suite failing)

--- src/test-suite_domain/TestSuite.cs
+++ src/test-suite_domain/TestSuite.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace SalesTaxes.TestSuite.Domain
 {
@@ -13,5 +15,34 @@ namespace SalesTaxes.TestSuite.Domain
             var receipt = counter.EmitReceipt();
             Assert.Equal(article.Price, receipt.Entries.SingleOrDefault()?.Price);
         }
+    }
+
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Counter
+    {
+        public void Scan(Article article)
+        {
+        }
+
+        public Receipt EmitReceipt()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Receipt
+    {
+        public IEnumerable<Entry> Entries { get; set; }
+    }
+
+    public class Entry
+    {
+        public decimal Price { get; set; }
     }
 }
