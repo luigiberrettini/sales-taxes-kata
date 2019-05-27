ee9cdbd9cdd021b76beec9405be909745a5db57f
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-27 23:54:35 +0100

    Remove test duplication (refactoring)

--- src/test-suite_domain/TestSuite.cs
+++ src/test-suite_domain/TestSuite.cs
@@ -7,26 +7,19 @@ namespace SalesTaxes.TestSuite.Domain
 {
     public class TestSuite
     {
-        [Fact]
-        public void OneBookIsExempt()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(5)]
+        [InlineData(100)]
+        public void NBooksAreExempt(int n)
         {
             var checkout = new Checkout();
             var article = new Article { Id = 1, Name = "Gone with the wind", Price = 25.0M };
-            checkout.Scan(article);
+            var priceForN = article.Price * n;
+            Enumerable.Range(1, n).ToList().ForEach(x => checkout.Scan(article));
             var receipt = checkout.EmitReceipt();
-            Assert.Equal(article.Price, receipt.Entries.SingleOrDefault()?.Price);
-        }
-
-        [Fact]
-        public void TwoBooksAreExempt()
-        {
-            var checkout = new Checkout();
-            var article = new Article { Id = 1, Name = "Gone with the wind", Price = 25.0M };
-            var priceForTwo = article.Price * 2;
-            checkout.Scan(article);
-            checkout.Scan(article);
-            var receipt = checkout.EmitReceipt();
-            Assert.Equal(priceForTwo, receipt.Entries.SingleOrDefault()?.Price);
+            Assert.Equal(priceForN, receipt.Entries.SingleOrDefault()?.Price);
         }
 
         [Fact]
