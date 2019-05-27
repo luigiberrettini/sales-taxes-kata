990cc40c9223ab452ae030194f8b770c799062f8
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-27 22:33:47 +0100

    Rename counter to checkout in test (refactoring)

--- src/test-suite_domain/TestSuite.cs
+++ src/test-suite_domain/TestSuite.cs
@@ -1,7 +1,6 @@
-﻿using SalesTaxes.Domain;
-using System.Linq;
-using SalesTaxes.Domain.Catalog;
+﻿using SalesTaxes.Domain.Catalog;
 using SalesTaxes.Domain.Shopping;
+using System.Linq;
 using Xunit;
 
 namespace SalesTaxes.TestSuite.Domain
@@ -11,10 +10,10 @@ namespace SalesTaxes.TestSuite.Domain
         [Fact]
         public void OneBookIsExempt()
         {
-            var counter = new Checkout();
+            var checkout = new Checkout();
             var article = new Article { Id = 1, Name = "Gone with the wind", Price = 25.0M };
-            counter.Scan(article);
-            var receipt = counter.EmitReceipt();
+            checkout.Scan(article);
+            var receipt = checkout.EmitReceipt();
             Assert.Equal(article.Price, receipt.Entries.SingleOrDefault()?.Price);
         }
     }
