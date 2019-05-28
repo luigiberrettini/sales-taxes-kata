6dd1f02a7bb3244ae9555b9763936c7dae2a5428
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-28 20:47:23 +0100

    Minimize tax object creations (refactoring)

--- src/domain/Taxation/TaxEngine.cs
+++ src/domain/Taxation/TaxEngine.cs
@@ -5,16 +5,20 @@ namespace SalesTaxes.Domain.Taxation
 {
     public class TaxEngine
     {
+        private static Tax NoTax { get; } = new Tax(0);
+
+        private static Tax BasicTax { get; } = new Tax(10);
+
         private readonly IDictionary<Category, Tax> _ratesByCategory;
 
         public TaxEngine()
         {
             _ratesByCategory = new Dictionary<Category, Tax>
             {
-                [Category.Books] = new Tax(0),
-                [Category.Food] = new Tax(0),
-                [Category.Medical] = new Tax(0),
-                [Category.Beauty] = new Tax(10)
+                [Category.Books] = NoTax,
+                [Category.Food] = NoTax,
+                [Category.Medical] = NoTax,
+                [Category.Beauty] = BasicTax
             };
         }
 
