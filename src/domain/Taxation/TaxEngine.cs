9249bf817ff0d2b58ed8d2ca7841f087be74aa04
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-28 20:11:12 +0100

    Make tax engine rely on a dictionary (refactoring)

--- src/domain/Taxation/TaxEngine.cs
+++ src/domain/Taxation/TaxEngine.cs
@@ -1,13 +1,24 @@
-﻿using SalesTaxes.Domain.Catalog;
+﻿using System.Collections.Generic;
+using SalesTaxes.Domain.Catalog;
 
 namespace SalesTaxes.Domain.Taxation
 {
     public class TaxEngine
     {
+        private IDictionary<Category, decimal> ratesByCategory;
+
+        public TaxEngine()
+        {
+            ratesByCategory = new Dictionary<Category, decimal>
+            {
+                [Category.Books] = 0,
+                [Category.Beauty] = 10
+            };
+        }
+
         public Tax TaxFor(Article article)
         {
-            var rate = article.Category == Category.Beauty ? 10 : 0;
-            return new Tax(rate);
+            return new Tax(ratesByCategory[article.Category]);
         }
     }
 }
