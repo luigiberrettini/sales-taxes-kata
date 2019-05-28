2a0fb20a4b9494cac5271010c4c1fa4b71134fe8
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-28 19:47:40 +0100

    Implement tax engine (test suite failing)

---
+++ src/domain/Taxation/TaxEngine.cs
@@ -0,0 +1,12 @@
+﻿using SalesTaxes.Domain.Catalog;
+
+namespace SalesTaxes.Domain.Taxation
+{
+    public class TaxEngine
+    {
+        public Tax TaxFor(Article article)
+        {
+            return new Tax(0);
+        }
+    }
+}
