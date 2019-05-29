5082d76e57e87a470c1c94634d6f56b7841d8391
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-29 23:24:23 +0100

    Test receipt to string (test suite failing)

--- src/test-suite_domain/PurchaseTestSuite.cs
+++ src/test-suite_domain/PurchaseTestSuite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using SalesTaxesKata.Domain;
 using SalesTaxesKata.Domain.Catalog;
 using SalesTaxesKata.Domain.Payment;
@@ -80,6 +81,33 @@ namespace SalesTaxesKata.TestSuite.Domain
             const decimal totalPriceWithTaxes = quantity * articlePrice;
             Assert.Equal($"{quantity} {articleName}: {totalPriceWithTaxes}", localReceipt.Entries.Single().ToString());
             Assert.Equal($"{quantity} imported {articleName}: {totalPriceWithTaxes}", importedReceipt.Entries.Single().ToString());
+        }
+
+        [Fact]
+        public void ReceiptToStringHasEntriesSalesTaxesAndTotal()
+        {
+            var supplier = new Supplier("VAT number", "Name", Country.Usa);
+            const string article1Name = "Article ABC";
+            const decimal article1Price = 32.54M;
+            const string article2Name = "Article ABC";
+            const decimal article2Price = 32.54M;
+            var article1 = new Article(1, supplier, Category.ArtsAndCrafts, article1Name, article1Price);
+            var article2 = new Article(1, supplier, Category.Baby, article2Name, article2Price);
+            var receipt = new Receipt();
+            var noTax = new NoTax();
+            var item1 = new Item(Country.Ita, article1, noTax);
+            var item2 = new Item(Country.Ita, article2, noTax);
+            receipt.Add(item1);
+            receipt.Add(item2);
+            var entry1 = new Entry(item1);
+            var entry2 = new Entry(item2);
+            var sb = new StringBuilder();
+            sb.AppendLine(entry1.ToString());
+            sb.AppendLine(entry2.ToString());
+            sb.AppendLine($"SalesTaxes: {receipt.SalesTaxes}");
+            sb.AppendLine($"Total: {receipt.Total}");
+            Assert.Equal(sb.ToString(), receipt.ToString());
+
         }
     }
 }
