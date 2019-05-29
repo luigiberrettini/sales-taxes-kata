e687d8b5dabd6b0901ca99b13d3931e53dfbc539
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-29 20:35:04 +0100

    Test that receipt for one article matches its data (not building)

--- src/test-suite_domain/PurchaseTestSuite.cs
+++ src/test-suite_domain/PurchaseTestSuite.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using SalesTaxesKata.Domain;
 using SalesTaxesKata.Domain.Catalog;
+using SalesTaxesKata.Domain.Payment;
 using SalesTaxesKata.Domain.Shopping;
 using SalesTaxesKata.Domain.Taxation;
 using Xunit;
@@ -30,6 +31,28 @@ namespace SalesTaxesKata.TestSuite.Domain
             purchase.Add(article);
             purchase.Add(article);
             Assert.NotNull(purchase.Items.SingleOrDefault());
+        }
+
+        [Fact]
+        public void ReceiptForOneArticleMatchesItsData()
+        {
+            var supplier = new Supplier("VAT number", "Name", Country.Ita);
+            var articleName = Guid.NewGuid().ToString();
+            var article = new Article(1, supplier, Category.ArtsAndCrafts, articleName, 100);
+            var purchase = new Purchase();
+            var tax = new BasicTax();
+            purchase.Add(article, tax);
+            var receipt = purchase.BuildReceipt();
+            var entry = receipt.Entries.Single();
+
+            const int quantity = 1;
+            var taxedPrice = tax.ApplyTo(article.Price);
+            var expectedEntry = new Entry(articleName, quantity, taxedPrice);
+            Assert.Equal(expectedEntry.Quantity, entry.Quantity);
+            Assert.Equal(expectedEntry.Name, entry.Name);
+            Assert.Equal(expectedEntry.Price, entry.Price);
+            Assert.Equal(expectedEntry.Price, receipt.SalesTaxes);
+            Assert.Equal(expectedEntry.Price, receipt.Total);
         }
     }
 }
