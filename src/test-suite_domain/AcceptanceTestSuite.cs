bf76c3ed07ad6d535cfc5b04e2bf0b08f63ce11d
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-31 00:01:06 +0100

    Test receipt for book, cd, chocolate (not building)

---
+++ src/test-suite_domain/AcceptanceTestSuite.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using SalesTaxesKata.Domain.Geo;
+using SalesTaxesKata.Domain.Sales;
+using SalesTaxesKata.Domain.Shopping;
+using SalesTaxesKata.Domain.Taxation;
+using Xunit;
+
+namespace SalesTaxesKata.TestSuite.Domain
+{
+    public class AcceptanceTestSuite
+    {
+        [Fact]
+        public void ReceiptForBookCdChocolate()
+        {
+            var catalog = new Catalog();
+            catalog.Add(new Article(1, Country.Ita, Category.Books, "book", 12.49M));
+            catalog.Add(new Article(2, Country.Ita, Category.Music, "music CD", 14.99M));
+            catalog.Add(new Article(3, Country.Ita, Category.Food, "chocolate bar", 0.85M));
+
+            var sb = new StringBuilder();
+            sb.AppendLine("1 book at 12.49");
+            sb.AppendLine("1 music CD at 14.99");
+            sb.AppendLine("1 chocolate bar at 0.85");
+            var input = sb.ToString();
+
+            sb.Clear();
+            sb.AppendLine("1 book: 12.49");
+            sb.AppendLine("1 music CD: 16.49");
+            sb.AppendLine("1 chocolate bar: 0.85");
+            sb.AppendLine("Sales Taxes: 1.50");
+            sb.AppendLine("Total: 29.83");
+            var output = sb.ToString();
+
+            var basket = Basket.FromString(input);
+            var checkout = new Checkout(Country.Ita, catalog, new TaxEngine());
+            basket.Goods.ForEach(checkout.Scan);
+            var receipt = checkout.EmitReceipt();
+            Assert.Equal(output, receipt.ToString());
+        }
+    }
+}
