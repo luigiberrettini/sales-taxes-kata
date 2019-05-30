8eb606c5c8b7ef6f1f79794f05117ced3c7573b7
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-30 21:15:30 +0100

    Implement basket (test suite passed)

---
+++ src/domain/Shopping/Basket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesTaxesKata.Domain.Shopping
+{
+    public class Basket : IEquatable<Basket>
+    {
+        private IReadOnlyCollection<Good> Goods { get; }
+
+        public static Basket FromString(string input)
+        {
+            var lines = input.Split((string[])new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var basket = new Basket();
+            lines.ToList().ForEach(x => basket.Add(Good.FromString(x)));
+            return basket;
+        }
+
+        public Basket()
+        {
+            Goods = new List<Good>();
+        }
+
+        public void Add(Good good)
+        {
+            ((IList<Good>)Goods).Add(good);
+        }
+
+        public override int GetHashCode()
+        {
+            return Goods == null ? 0 : Goods.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            return ReferenceEquals(this, obj) ||
+                   obj.GetType() == this.GetType() && Equals((Basket)obj);
+        }
+
+        public bool Equals(Basket other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            return ReferenceEquals(this, other) ||
+                   Goods.SequenceEqual(other.Goods);
+        }
+    }
+}
