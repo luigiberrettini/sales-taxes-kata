012dae79799f7be7ebaebcaf0db9f146c43d5194
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-05-30 19:38:31 +0100

    Test good import from string (build failing)

---
+++ src/test-suite_domain/BasketTestSuite.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace SalesTaxesKata.TestSuite.Domain
+{
+    public class BasketTestSuite
+    {
+        [Fact]
+        public void GoodFromStringFormatIsQuantityNameAtShelfPrice()
+        {
+            const string name = "Article ABC";
+            const int quantity = 2;
+            const decimal shelfPrice = 10M;
+            var builtGood = new Good(name, quantity, shelfPrice);
+            var importedGood = Good.FromString($"{quantity} {name} at {shelfPrice}");
+            Assert.Equal(builtGood, importedGood);
+        }
+    }
+}
